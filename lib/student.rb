91676ad62755500a063bdc2f42d35761f8fd344e
Robert Laws <roblaws@me.com>
2017-10-10 19:41:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,15 +25,16 @@ class Student
   def self.drop_table
     sql = <<-SQL
       DROP TABLE students
-      SQL
+    SQL
 
     DB[:conn].execute(sql)
   end
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) VALUES (?, ?)
-      SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
