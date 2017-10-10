6c83e225a2ec1ef14bd6cc6134b873295384be38
Robert Laws <roblaws@me.com>
2017-10-10 19:30:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,5 @@
 class Student
-  
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
@@ -7,7 +7,8 @@ class Student
   attr_reader :id
 
   def initialize(name, grade, id = nil)
-
+    @name = name
+    @grade = grade
   end
 
 end
