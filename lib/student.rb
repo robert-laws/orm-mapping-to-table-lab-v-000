543cdcd2dfeeb16a69cc68616f4f61d006ada868
Robert Laws <roblaws@me.com>
2017-10-10 19:29:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,13 @@
 class Student
-
+  
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
+
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(name, grade, id = nil)
 
   end
-  
+
 end
